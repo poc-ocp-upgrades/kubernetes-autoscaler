a52ecb73ffd3064936a13e1c89026804a195e261
Jan Chaloupka <jchaloup@redhat.com>
2018-04-20 13:25:22 +0200

    Add openshift/release Makefile and hack scripts
    Signed-off-by: Jan Chaloupka jchaloup@redhat.com

--- hack/verify-golint.sh
+++ hack/verify-golint.sh
@@ -1,47 +1,34 @@
 #!/bin/bash
+source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
-# Copyright 2014 The Kubernetes Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+os::golang::verify_go_version
+os::util::ensure::system_binary_exists 'golint'
 
-set -o errexit
-set -o nounset
-set -o pipefail
+arg="${1:-""}"
+bad_files=""
 
-KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
-
-GO_VERSION=($(go version))
-# golint only works for golang 1.5+
-if [[ -n $(echo "${GO_VERSION[2]}" | grep -E 'go1.1|go1.2|go1.3|go1.4') ]]; then
-  echo "GOLINT requires go 1.5+. Skipping"
-  exit
+if [ "$arg" == "-m" ]; then
+	head=$(git rev-parse --short HEAD | xargs echo -n)
+	set +e
+	modified_files=$(git diff-tree --no-commit-id --name-only -r master..$head | \
+		grep "^pkg" | grep ".go$" | grep -v "bindata.go$" | grep -v "Godeps" | \
+		grep -v "third_party")
+	if [ -n "${modified_files}" ]; then
+		echo -e "Checking modified files: ${modified_files}\n"
+		for f in $modified_files; do golint $f; done
+		echo
+	fi
+	set -e
+else
+	bad_files=$(os::util::list_go_src_files | \
+		sort -u | \
+		sed 's/^.{2}//' | \
+		xargs -n1 printf "${GOPATH}/src/${OS_GO_PACKAGE}/%s\n" | \
+		xargs -n1 golint)
 fi
 
-cd "${KUBE_ROOT}"
-
-GOLINT=${GOLINT:-"golint"}
-PACKAGES=($(go list ./... | grep -v /vendor/ | grep -v vertical-pod-autoscaler/pkg/client | grep -v vertical-pod-autoscaler/pkg/apis))
-bad_files=()
-for package in "${PACKAGES[@]}"; do
-  out=$("${GOLINT}" -min_confidence=0.9 "${package}")
-  if [[ -n "${out}" ]]; then
-    bad_files+=("${out}")
-  fi
-done
-if [[ "${#bad_files[@]}" -ne 0 ]]; then
-  echo "!!! '$GOLINT' problems: "
-  echo "${bad_files[@]}"
-  exit 1
+if [[ -n "${bad_files}" ]]; then
+	echo "golint detected following problems:"
+	echo "${bad_files}"
+	exit 1
 fi
-
-# ex: ts=2 sw=2 et filetype=sh
