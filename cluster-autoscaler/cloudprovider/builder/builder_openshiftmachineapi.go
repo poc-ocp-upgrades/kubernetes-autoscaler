07edb0b1b0310dbd1907b9c7bb78d1172f3ef3d6
Andrew McDermott <amcdermo@redhat.com>
2019-03-27 13:02:03 +0000

    UPSTREAM: <carry>: openshift: cloudprovider builder updates for 1.13

--- cluster-autoscaler/cloudprovider/builder/builder_openshiftmachineapi.go
+++ cluster-autoscaler/cloudprovider/builder/builder_openshiftmachineapi.go
@@ -1,7 +1,7 @@
-// +build !gce,!aws,!azure,!kubemark,!alicloud,!openshiftmachineapi
+// +build openshiftmachineapi
 
 /*
-Copyright 2018 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -20,42 +20,23 @@ package builder
 
 import (
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/alicloud"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/aws"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/azure"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gke"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/openshiftmachineapi"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 )
 
 // AvailableCloudProviders supported by the cloud provider builder.
 var AvailableCloudProviders = []string{
-	aws.ProviderName,
-	azure.ProviderName,
-	gce.ProviderNameGCE,
-	gke.ProviderNameGKE,
-	alicloud.ProviderName,
 	openshiftmachineapi.ProviderName,
 }
 
-// DefaultCloudProvider is GCE.
-const DefaultCloudProvider = gce.ProviderNameGCE
+// DefaultCloudProvider for machineapi-only build.
+const DefaultCloudProvider = openshiftmachineapi.ProviderName
 
 func buildCloudProvider(opts config.AutoscalingOptions, do cloudprovider.NodeGroupDiscoveryOptions, rl *cloudprovider.ResourceLimiter) cloudprovider.CloudProvider {
 	switch opts.CloudProviderName {
-	case gce.ProviderNameGCE:
-		return gce.BuildGCE(opts, do, rl)
-	case gke.ProviderNameGKE:
-		return gke.BuildGKE(opts, do, rl)
-	case aws.ProviderName:
-		return aws.BuildAWS(opts, do, rl)
-	case azure.ProviderName:
-		return azure.BuildAzure(opts, do, rl)
-	case alicloud.ProviderName:
-		return alicloud.BuildAlicloud(opts, do, rl)
 	case openshiftmachineapi.ProviderName:
 		return openshiftmachineapi.BuildOpenShiftMachineAPI(opts, do, rl)
 	}
+
 	return nil
 }
